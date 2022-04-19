f751486ff2a2c461f48ec9d0efbdb2bd6bf0423f
hbarrons <hbarrons@gmail.com>
2022-04-19 19:57:19 -0400

    solve problem 4

--- challenges.js
+++ challenges.js
@@ -212,6 +212,21 @@ The function plusMinus should return an array like this:
 -----------------------------------------------------------------------------*/
 // Your solution for 04- here:
 
+function plusMinus (arr) {
+  let positive = 0
+  let negative = 0
+  let zero = 0
+  arr.forEach(num => {
+    if (num > 0) {
+      positive += 1
+    } else if (num < 0) {
+      negative += 1
+    } else {
+      zero += 1
+    }
+  })
+  return [(positive/arr.length).toFixed(6), (negative/arr.length).toFixed(6), (zero/arr.length).toFixed(6)]
+}
 
 
 
